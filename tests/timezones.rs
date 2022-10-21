89aca39b1d7c642bf52170400637770651aaabfd
Viktor Kuroljov <viktor@ku.family>
2022-10-21 11:04:46 +0300

    support "-" sign in utc expr

--- tests/timezones.rs
+++ tests/timezones.rs
@@ -15,4 +15,13 @@ mod timezones {
             long => "at 10am utc+0400" match duration!{05:00},
         }
     }
+
+    test! {
+        name: minus_sign,
+        now: time! {15:00 UTC_PLUS_3},
+        offset: UTC_PLUS_3,
+        variants: {
+            at_10am_utc_m_3 => "at 8pm utc-3" match duration!{11:00},
+        }
+    }
 }
